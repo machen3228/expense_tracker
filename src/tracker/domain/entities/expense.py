4de9b9874f655fdb2ebfaf5ce546df6411275925
machen <machen3228@gmail.com>
2026-04-13 22:42:19 +0300

    feat: tie categories with income and expense

--- src/tracker/domain/entities/expense.py
+++ src/tracker/domain/entities/expense.py
@@ -8,6 +8,7 @@ from tracker.domain.entities.base import entity
 if TYPE_CHECKING:
     from datetime import datetime
 
+    from tracker.domain.entities.expense_category import ExpenseCategoryId
     from tracker.domain.entities.person import PersonId
     from tracker.domain.values.dough import Dough
 
@@ -17,6 +18,7 @@ ExpenseId = NewType("ExpenseId", UUID)
 @entity
 class Expense(Entity[ExpenseId]):
     amount: Dough
+    category: ExpenseCategoryId
     description: str
     paid_by: PersonId
     created_at: datetime
--- src/tracker/domain/entities/income.py
+++ src/tracker/domain/entities/income.py
@@ -8,6 +8,7 @@ from tracker.domain.entities.base import entity
 if TYPE_CHECKING:
     from datetime import datetime
 
+    from tracker.domain.entities.income_category import IncomeCategoryId
     from tracker.domain.entities.person import PersonId
     from tracker.domain.values.dough import Dough
 
@@ -17,6 +18,7 @@ IncomeId = NewType("IncomeId", UUID)
 @entity
 class Income(Entity[IncomeId]):
     amount: Dough
+    category: IncomeCategoryId
     description: str
     received_by: PersonId
     created_at: datetime
