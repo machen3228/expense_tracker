12804886e80de87a97a36e3710bf22ccbe6faed7
machen <machen3228@gmail.com>
2026-04-17 23:48:18 +0300

    fix: update CreatePersonResponse

---
+++ src/tracker/application/dto/response/person/base.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+from typing import Self
+
+from tracker.application.dto.base import dto
+
+if TYPE_CHECKING:
+    from tracker.domain.entities.person import Person
+    from tracker.domain.entities.person import PersonId
+
+
+@dto
+class PersonView:
+    id: PersonId
+    username: str
+
+    @classmethod
+    def from_domain(cls, entity: Person) -> Self:
+        return cls(
+            id=entity.id,
+            username=entity.username.value,
+        )
