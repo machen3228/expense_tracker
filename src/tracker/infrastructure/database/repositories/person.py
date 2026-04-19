2680f7e575d7eb1b7573244ff06ec8d77a05b23e
machen <machen3228@gmail.com>
2026-04-19 23:14:58 +0300

    feat: add PersonORM

---
+++ src/tracker/infrastructure/database/repositories/person.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from adaptix import P
+from adaptix.conversion import link
+
+from tracker.domain.entities.person import Person
+from tracker.infrastructure.database.mapper import get_mapper
+from tracker.infrastructure.database.repositories.base import SAAbstractRepository
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+_to_orm: Callable[[Person], PersonORM] = get_mapper(
+    Person,
+    PersonORM,
+    recipe=[
+        link(P[Person].username, P[PersonORM].username, coercer=lambda u: u.value),
+    ],
+)
+
+
+class SAPersonRepository(SAAbstractRepository[Person, PersonORM]):
+    def _to_orm(self, entity: Person) -> PersonORM:
+        return _to_orm(entity)
