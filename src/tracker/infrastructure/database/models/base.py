881db8e06271cff2dc47c66508d2ee274fea0e45
machen <machen3228@gmail.com>
2026-04-19 22:37:11 +0300

    feat: add BaseORM

---
+++ src/tracker/infrastructure/database/models/base.py
@@ -0,0 +1,18 @@
+from typing import ClassVar
+
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+
+metadata = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+
+class BaseORM(DeclarativeBase):
+    metadata: ClassVar[MetaData] = metadata
