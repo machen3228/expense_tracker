205788140c08c34a5b0bf0b0359ad917a494b725
machen <machen3228@gmail.com>
2026-04-26 18:57:41 +0300

    feat: add get_current_user_token

---
+++ src/tracker/presentation/api/dependencies.py
@@ -0,0 +1,8 @@
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+async def get_current_user_token(token: str = Depends(oauth2_scheme)) -> str:
+    return token
