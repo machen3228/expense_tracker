0f0bdb49f1411ed116b99dd22c91ae404c78b7f7
machen <machen3228@gmail.com>
2026-04-26 18:38:21 +0300

    feat: add ChangePersonName to interactor provider

--- src/tracker/main/di/providers/interactors.py
+++ src/tracker/main/di/providers/interactors.py
@@ -3,9 +3,11 @@ from dishka import Provider
 from dishka import Scope
 from dishka import provide
 
+from tracker.application.commands.person.change_username import ChangePersonName
 from tracker.application.commands.person.create import CreatePerson
 from tracker.application.interfaces.readers.person import IPersonReader
 from tracker.application.interfaces.repositories.person import IPersonRepository
+from tracker.application.interfaces.security.identity_provider import IIdentityProvider
 from tracker.application.interfaces.security.password_hasher import IPasswordHasher
 from tracker.application.interfaces.transaction_manager import ITransactionManager
 
@@ -26,4 +28,19 @@ class InteractorsProvider(Provider):
             person_reader,
             password_hasher,
             transaction_manager,
+        )
+
+    @provide
+    def change_username(
+        self,
+        person_repository: IPersonRepository,
+        person_reader: IPersonReader,
+        transaction_manager: ITransactionManager,
+        identity_provider: IIdentityProvider,
+    ) -> ChangePersonName:
+        return ChangePersonName(
+            person_repository,
+            person_reader,
+            transaction_manager,
+            identity_provider,
         )
