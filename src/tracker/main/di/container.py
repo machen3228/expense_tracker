d1250552be6a2f9437b8b55a34db7c2b8f90d90f
machen <machen3228@gmail.com>
2026-04-26 18:29:07 +0300

    feat: create SecurityProvider

--- src/tracker/main/di/container.py
+++ src/tracker/main/di/container.py
@@ -3,9 +3,10 @@ from typing import TYPE_CHECKING
 from dishka import make_async_container
 
 from tracker.config.database import DatabaseConfig
+from tracker.config.jwt import JWTConfig
 from tracker.main.di.providers.database import DatabaseProvider
 from tracker.main.di.providers.interactors import InteractorsProvider
-from tracker.main.di.providers.password_hasher import PasswordHasherProvider
+from tracker.main.di.providers.password_hasher import SecurityProvider
 from tracker.main.di.providers.readers import ReadersProvider
 from tracker.main.di.providers.repositories import RepositoriesProvider
 
@@ -20,13 +21,14 @@ def create_container(config: Config) -> AsyncContainer:
     providers: list[Provider] = [
         DatabaseProvider(),
         InteractorsProvider(),
-        PasswordHasherProvider(),
+        SecurityProvider(),
         ReadersProvider(),
         RepositoriesProvider(),
     ]
 
     context: dict[type, object] = {
         DatabaseConfig: config.database,
+        JWTConfig: config.jwt,
     }
 
     return make_async_container(*providers, context=context)
--- src/tracker/main/di/providers/password_hasher.py
+++ src/tracker/main/di/providers/password_hasher.py
@@ -2,14 +2,44 @@ from dishka import BaseScope
 from dishka import Provider
 from dishka import Scope
 from dishka import provide
+from fastapi import Request
 
+from tracker.application.interfaces.readers.person import IPersonReader
+from tracker.application.interfaces.security.identity_provider import IIdentityProvider
+from tracker.application.interfaces.security.jwt_provider import IJWTProvider
 from tracker.application.interfaces.security.password_hasher import IPasswordHasher
+from tracker.config.jwt import JWTConfig
+from tracker.infrastructure.security.identity_provider import IdentityProvider
+from tracker.infrastructure.security.jwy_provider import PyJWTProvider
 from tracker.infrastructure.security.password_hasher import BcryptPasswordHasher
 
 
-class PasswordHasherProvider(Provider):
+class SecurityProvider(Provider):
     scope: BaseScope | None = Scope.APP
 
     @provide
     def password_hasher(self) -> IPasswordHasher:
         return BcryptPasswordHasher()
+
+    @provide
+    def jwt_provider(self, config: JWTConfig) -> IJWTProvider:
+        return PyJWTProvider(
+            secret_key=config.secret_key.get_secret_value(),
+            algorithm=config.algorithm,
+            access_ttl=config.access_ttl_minutes,
+            refresh_ttl=config.refresh_ttl_minutes,
+        )
+
+    @provide(scope=Scope.REQUEST)
+    def identity_provider(
+        self,
+        request: Request,
+        jwt_provider: IJWTProvider,
+        person_reader: IPersonReader,
+    ) -> IIdentityProvider:
+        token: str = request.headers.get("Authorization", "").removeprefix("Bearer ")
+        return IdentityProvider(
+            token=token,
+            jwt_provider=jwt_provider,
+            person_reader=person_reader,
+        )
