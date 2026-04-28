247510f34d186e4b9ae4e02e979a8fafa0419d04
machen <machen3228@gmail.com>
2026-04-28 23:24:55 +0300

    feat: add refresh router

--- src/tracker/presentation/api/routers/auth.py
+++ src/tracker/presentation/api/routers/auth.py
@@ -7,8 +7,11 @@ from fastapi import Form
 from fastapi import status
 
 from tracker.application.commands.auth.login import LoginPerson
+from tracker.application.commands.auth.refresh_token import RefreshToken
 from tracker.application.dto.request.auth.login import LoginRequest
+from tracker.application.dto.request.auth.refresh_token import RefreshTokenRequest
 from tracker.application.dto.response.auth.login import LoginResponse
+from tracker.application.dto.response.auth.refresh_token import RefreshTokenResponse
 
 router = APIRouter(prefix="/auth", tags=["Auth"], route_class=DishkaRoute)
 
@@ -18,4 +21,12 @@ async def login(
     request: Annotated[LoginRequest, Form()],
     interactor: FromDishka[LoginPerson],
 ) -> LoginResponse:
+    return await interactor.execute(request)
+
+
+@router.post("/refresh", status_code=status.HTTP_200_OK)
+async def refresh_token(
+    request: Annotated[RefreshTokenRequest, Form()],
+    interactor: FromDishka[RefreshToken],
+) -> RefreshTokenResponse:
     return await interactor.execute(request)
